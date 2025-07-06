a1e7ecf95235337e13d1845f63e6595250e305ef
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-06 10:52:51 +0530

    Title component added and cards added in admin dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -2,6 +2,8 @@ import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from '
 import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets'
 import Loading from '../../components/Loading'
+import Title from '../../components/admin/Title'
+import BlurCircle from '../../components/BlurCircle'
 
 const Dashboard = () => {
 
@@ -18,7 +20,7 @@ const Dashboard = () => {
 
     const dashboardCards = [
         { title: "Total Bookings", value: dashboardData.totalBookings || "0", icon: ChartLineIcon },
-        { title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
+        { title: "Total Revenue", value: currency + dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
         { title: "Active Shows", value: dashboardData.activeShows.length || "0", icon: PlayCircleIcon },
         { title: "Total Users", value: dashboardData.totalUser || "0", icon: UsersIcon },
     ]
@@ -33,9 +35,24 @@ const Dashboard = () => {
     }, [])
 
   return !loading ? (
-    <div>
-      
-    </div>
+    <>
+      <Title text1="Admin" text2="Dashboard" />
+
+      <div className='relative flex flex-wrap gap-4 mt-6'>
+        <BlurCircle top='-100px' left='0' />
+        <div className='flex flex-wrap gap-4 w-full'>
+            {dashboardCards.map((card, index) => (
+                <div key={index} className='flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full'>
+                    <div>
+                        <h1 className='text-sm'>{card.title}</h1>
+                        <p className='text-xl font-medium mt-1'>{card.value}</p>
+                    </div>
+                    <card.icon className='w-6 h-6' />
+                </div>
+            ))}
+        </div>
+      </div>
+    </>
   ) : <Loading />
 }
 
