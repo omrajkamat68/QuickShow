86956817fcac7bf41d3edbce69896954f5ce9832
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-10 11:16:22 +0530

    Controller function to get occupied seats , and booking routes added

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -19,41 +19,56 @@ const checkSeatsAvailability = async (showId, selectedSeats) => {
 };
 
 export const createBooking = async (req, res) => {
-    try {
-        const {userId} = req.auth();
-        const {showId, selectedSeats} = req.body
-        const {origin} = req.headers
+  try {
+    const { userId } = req.auth();
+    const { showId, selectedSeats } = req.body;
+    const { origin } = req.headers;
 
-        const isAvailable = await checkSeatsAvailability(showId, selectedSeats)
+    const isAvailable = await checkSeatsAvailability(showId, selectedSeats);
 
-        if(!isAvailable){
-            return res.json({success: false, message: "Selected Seats are not available."})
-        }
-
-        const showData = await Show.findById(showId).populate('movie')
-
-        const booking = await Booking.create({
-            user: userId,
-            show: showId,
-            amount: showData.showPrice * selectedSeats.length,
-            bookedSeats: selectedSeats
-        })
-
-        selectedSeats.map((seat)=>{
-            showData.occupiedSeats[seat] = userId
-        })
-
-        showData.markModified('occupiedSeats')
-
-        await showData.save()
-
-        // Stripe Gateway Initialize
-
-        res.json({success: true, message: 'Booked successfully'})
-
-
-    } catch (error) {
-        console.log(error.message);
-        res.json({success: false, message: error.message})
+    if (!isAvailable) {
+      return res.json({
+        success: false,
+        message: "Selected Seats are not available.",
+      });
     }
-}
+
+    const showData = await Show.findById(showId).populate("movie");
+
+    const booking = await Booking.create({
+      user: userId,
+      show: showId,
+      amount: showData.showPrice * selectedSeats.length,
+      bookedSeats: selectedSeats,
+    });
+
+    selectedSeats.map((seat) => {
+      showData.occupiedSeats[seat] = userId;
+    });
+
+    showData.markModified("occupiedSeats");
+
+    await showData.save();
+
+    // Stripe Gateway Initialize
+
+    res.json({ success: true, message: "Booked successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getOccupiedSeats = async (req, res) => {
+  try {
+    const { showId } = req.params;
+    const showData = await Show.findById(showId);
+
+    const occupiedSeats = Object.keys(showData.occupiedSeats);
+
+    res.json({ success: true, occupiedSeats });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
