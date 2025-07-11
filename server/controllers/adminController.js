26ba8946d3a44d41ed5de68e5c878d416df7561e
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-11 12:36:13 +0530

    Controller functions to get all shows and get all bookings and admin routes added.

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -30,3 +30,33 @@ export const getDashboardData = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// API to get all shows
+export const getAllShows = async (req, res) => {
+  try {
+    const shows = await Show.find({ showDateTime: { $gte: new Date() } })
+      .populate("movie")
+      .sort({ showDateTime: 1 });
+    res.json({ success: true, shows });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get all bookings
+export const getAllBookings = async (req, res) => {
+  try {
+    const bookings = await Booking.find({})
+      .populate("user")
+      .populate({
+        path: "show",
+        populate: { path: "movie" },
+      })
+      .sort({ createdAt: -1 });
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
