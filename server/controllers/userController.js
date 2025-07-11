da5a0f28a84e379e5737c84d2e6d2db665844a10
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-11 13:16:00 +0530

    Controller functions to get user bookings and to add favorite movie in clerk user metadata added.

---
+++ server/controllers/userController.js
@@ -0,0 +1,47 @@
+import { clerkClient } from "@clerk/express";
+import Booking from "../models/Booking.js";
+
+// API controller function to get user bookings
+export const getUserBookings = async (req, res) => {
+  try {
+    const user = req.auth().userId;
+
+    const bookings = await Booking.find({ user })
+      .populate({
+        path: "show",
+        populate: { path: "movie" },
+      })
+      .sort({ createdAt: -1 });
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API Controller function to add favorite movie in clerk user metadata
+export const addFavorite = async (req, res) => {
+  try {
+    const { movieId } = req.body;
+    const userId = req.auth().userId;
+
+    const user = await clerkClient.users.getUser(userId);
+
+    if (!user.privateMetadata.favorites) {
+      user.privateMetadata.favorites = [];
+    }
+
+    if (!user.privateMetadata.favorites.includes(movieId)) {
+      user.privateMetadata.favorites.push(movieId);
+    }
+
+    await clerkClient.users.updateUserMetadata(userId, {
+      privateMetadata: user.privateMetadata,
+    });
+
+    res.json({ success: true, message: "Favorite added successfully." });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
