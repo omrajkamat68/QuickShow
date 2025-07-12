ded373f12cb531ac39bb8d2ce7c6b81e76907fce
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-12 13:49:33 +0530

    Function to fetch admin added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,8 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios"
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
@@ -12,7 +15,34 @@ export const AppProvider = ({ children })=>{
     const [shows, setShows] = useState([])
     const [favoriteMovies, setFavoriteMovies] = useState([])
 
+    const {user} = useUser()
+    const {getToken} = useAuth()
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const fetchIsAdmin = async () => {
+        try {
+            const {data} = await axios.get('/api/admin/is-admin', {headers: {Authorization: `Bearer ${await getToken()}`}})
+            setIsAdmin(data.isAdmin)
+
+            if(!data.isAdmin && location.pathname.startsWith('/admin')){
+                navigate('/')
+                toast.error('You are not authorized to access admin dashboard.')
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(()=>{
+        if(user){
+            fetchIsAdmin()
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[user])
+
     const value = {axios}
+
     return (
         <AppContext.Provider value={value}>
             { children }
