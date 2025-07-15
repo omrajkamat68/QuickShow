bc040ca65221ae99c68b740350be1031e8905f14
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-15 14:00:38 +0530

    Loading component and book tickets function updated.

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Loading = () => {
-  return (
-    <div className='flex justify-center items-center h-[80vh]'>
-      <div className='animate-spin rounded-full h-14 w-14 border-2 border-t-primary'></div>
-    </div>
-  )
-}
+  const { nextUrl } = useParams();
+  const navigate = useNavigate();
 
-export default Loading
+  useEffect(() => {
+    if (nextUrl) {
+      setTimeout(() => {
+        navigate("/" + nextUrl);
+      }, 8000);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center h-[80vh]">
+      <div className="animate-spin rounded-full h-14 w-14 border-2 border-t-primary"></div>
+    </div>
+  );
+};
+
+export default Loading;
