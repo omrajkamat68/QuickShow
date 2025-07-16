ba4ac7bdfc008e77bfa7fb6d8af7366bed949e3f
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-16 16:16:08 +0530

    Inngest Function to send email when user books a show added.

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -73,6 +73,20 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
     }
 )
 
+// Inngest Function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+    {id: "send-booking-confirmation-email"},
+    {event: "app/show.booked"},
+    async ({ event, step }) => {
+        const { bookingId } = event.data;
+
+        const booking = await Booking.findById(bookingId).populate({
+            path: 'show',
+            populate: {path: "movie", model: "Movie"}
+        }).populate('user');
+    }
+)
+
 export const functions = [
     syncUserCreation,
     syncUserDeletion,
